af8ec6a26ae5d11331b1c2a406c150b478802b05
Debojit Sinha <sinha.debojit@gmail.com>
2019-05-25 18:42:56 +0300

    Initial project setup

---
+++ src/test/java/nom/side/poc/cache/AppTest.java
@@ -0,0 +1,20 @@
+package nom.side.poc.cache;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
